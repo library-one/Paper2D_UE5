efe958c0da4617deee534faa02fe19a0f68d33cd
library-one <tjwodnjs98@gmail.com>
2024-07-05 12:09:36 +0900

    sdf
    sdf

---
+++ Source/Paper2D_UE5/OptionsWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OptionsWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPER2D_UE5_API UOptionsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
